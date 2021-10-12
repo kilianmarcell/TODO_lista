4c15c38d3017accee79e95659053c6ef9827d874
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-12 16:00:48 +0200

    Lista létrehozva, új adattagokat hozzá lehet adni, beviteli mező gomb nyomásakor üres lesz

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ function kattint() {
     let szoveg = document.getElementById("bevitel").value;
     document.getElementById("bevitel").value = "";
     lista.push(szoveg);
-    for (let i = 0; i < lista.length; i++) {
-        console.log(lista[i]);
-    }
 }
 
 function init() {
